e7e828f56912acb7febd3c4573e0d99a30a7bb91
Shalom Bharathi <shalombharathi@gmail.com>
2025-01-26 18:20:34 +0530

    Update app.js

--- diets/generate-diets/app.js
+++ diets/generate-diets/app.js
@@ -1,19 +1,17 @@
 // Initialize Firebase if not already initialized
-if (!firebase.apps.length) {
-  const firebaseConfig = {
-    apiKey: "AIzaSyAtBxeZrh4cej7ZzsKZ5uN-BqC_wxoTmdE",
-    authDomain: "coreai-82c79.firebaseapp.com",
-    databaseURL: "https://coreai-82c79-default-rtdb.firebaseio.com",
-    projectId: "coreai-82c79",
-    storageBucket: "coreai-82c79.firebasestorage.app",
-    messagingSenderId: "97395011364",
-    appId: "1:97395011364:web:1e8f6a06fce409bfd80db1",
-    measurementId: "G-0J1RLMVEGC"
-  };
 
-  firebase.initializeApp(firebaseConfig);
-  console.log("Firebase initialized");
-}
+const firebaseConfig = {
+  apiKey: "AIzaSyAtBxeZrh4cej7ZzsKZ5uN-BqC_wxoTmdE",
+  authDomain: "coreai-82c79.firebaseapp.com",
+  databaseURL: "https://coreai-82c79-default-rtdb.firebaseio.com",
+  projectId: "coreai-82c79",
+  storageBucket: "coreai-82c79.firebasestorage.app",
+  messagingSenderId: "97395011364",
+  appId: "1:97395011364:web:1e8f6a06fce409bfd80db1",
+  measurementId: "G-0J1RLMVEGC"
+};
+firebase.initializeApp(firebaseConfig);
+console.log("Firebase initialized");
 
 // Initialize Firebase services
 const db = firebase.firestore();
