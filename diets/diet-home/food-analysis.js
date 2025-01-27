5cce94d6e375e26bc5f2c9715a54afbf445a93af
Shalom Bharathi <shalombharathi@gmail.com>
2025-01-28 00:45:10 +0530

    fix

--- diets/diet-home/food-analysis.js
+++ diets/diet-home/food-analysis.js
@@ -1,6 +1,18 @@
 let stream = null;
 let selectedImage = null;
 
+let API_KEY;
+let thingsRefx;
+let unsubscribex;
+let db = firebase.firestore();
+thingsRefx = db.collection('API');
+
+unsubscribex = thingsRefx.onSnapshot(querySnapshot => {
+  querySnapshot.docs.forEach(doc => {
+    API_KEY = doc.data().API;
+  });
+});
+
 // Camera handling
 window.toggleCamera = async () => {
   if (stream) {
